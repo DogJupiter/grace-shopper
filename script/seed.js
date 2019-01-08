940863850a45ed17ff45f7ddf102bbe7eb62229f
Wenyi Zheng <hilarywenyi@gmail.com>
2019-01-08 15:49:56 -0500

    feat: added seed file in script/seed.js

--- script/seed.js
+++ script/seed.js
@@ -1,19 +1,108 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Experience, Review, Order} = require('../server/db/models')
 
-async function seed() {
-  await db.sync({force: true})
-  console.log('db synced!')
+//Experiences Data
+const experiences = [
+  {
+    name: 'Jane Hotel After Midnight',
+    imageUrl:
+      'https://static.urbandaddy.com/uploads/assets/image/articles/standard/cf4eb440aea70a5e56c2453b9e3ce2101debb11e.jpg',
+    category: 'drink',
+    duration: '2 hours',
+    price: 50,
+    description: 'night out'
+  },
+  {
+    name: 'Ballet in Lincoln Center',
+    imageUrl:
+      'https://static.urbandaddy.com/uploads/assets/image/articles/standard/cf4eb440aea70a5e56c2453b9e3ce2101debb11e.jpg',
+    category: 'sports',
+    duration: '2 hours',
+    price: 80,
+    description: 'swam lake'
+  },
+  {
+    name: 'Street Food in Flushing',
+    imageUrl:
+      'https://static.urbandaddy.com/uploads/assets/image/articles/standard/cf4eb440aea70a5e56c2453b9e3ce2101debb11e.jpg',
+    category: 'food',
+    duration: '2 hours',
+    price: 40,
+    description: 'eat eat eat'
+  }
+]
+//User Data
+const users = [
+  {
+    firstName: 'Koby',
+    lastName: 'Bryant',
+    email: 'Koby@email.com',
+    password: '123',
+    imageUrl: 'https://timedotcom.files.wordpress.com/2014/11/458357166.jpg',
+    googleId: null
+  },
+  {
+    firstName: 'Kevin',
+    lastName: 'Durant',
+    email: 'Kevin@email.com',
+    password: '123',
+    imageUrl:
+      'https://s.newsweek.com/sites/www.newsweek.com/files/styles/full/public/2017/12/05/golden-state-warriors-forward-kevin-durant..jpg',
+    googleId: null
+  },
+  {
+    firstName: 'Stephen',
+    lastName: 'Curry',
+    email: 'Curry@email.com',
+    password: '123',
+    imageUrl:
+      'http://static1.uk.businessinsider.com/image/56c770d5dd08958e6d8b463c-1190-625/stephen-curry-heres-a-look-at-the-marvelous-life-of-the-greatest-basketball-player-in-the-world.jpg',
+    googleId: null
+  }
+]
+//Review Data
+const reviews = [
+  {description: 'I love this experiences!'},
+  {description: 'I love this experiences!'},
+  {description: 'I love this experiences!'}
+]
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+//Order Data
+const orders = [{quantity: []}, {quantity: []}, {quantity: []}]
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+const seed = async () => {
+  await db
+    .sync({force: true})
+    .then(() => {
+      return Promise.all(
+        experiences.map(experience => {
+          return Experience.create(experience)
+        })
+      )
+    })
+    .then(() => {
+      return Promise.all(
+        users.map(user => {
+          return User.create(user)
+        })
+      )
+    })
+    .then(() => {
+      return Promise.all(
+        orders.map(order => {
+          return Order.create(order)
+        })
+      )
+    })
+    .then(() => {
+      return Promise.all(
+        reviews.map(review => {
+          return Review.create(review)
+        })
+      )
+    })
 }
 
 // We've separated the `seed` function from the `runSeed` function.
@@ -23,6 +112,7 @@ async function runSeed() {
   console.log('seeding...')
   try {
     await seed()
+    console.log('seeding success!')
   } catch (err) {
     console.error(err)
     process.exitCode = 1
--- server/db/models/review.js
+++ server/db/models/review.js
@@ -4,13 +4,7 @@ const db = require('../db')
 const Review = db.define('review', {
   description: {
     type: Sequelize.TEXT,
-    allowNull: false,
-    validate: {
-      len: {
-        args: 50,
-        message: 'review text must be at least 50 characters in length'
-      }
-    }
+    allowNull: false
   }
 })
 
