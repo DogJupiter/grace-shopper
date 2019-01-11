6c7060d39438ca69e6913e309c50393b4543616b
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2019-01-11 12:43:32 -0500

    Adding and fetching cart works. Pulling from master to fix merge conflicts

--- client/components/Cart-Item-Summary.js
+++ client/components/Cart-Item-Summary.js
@@ -1,39 +1,57 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-const CartItemSummary = props => {
-  console.log(props)
-  return (
-    <div>
-      <div className="cart-item">
-        <div className="cart-item-img">
-          {/* Product Image appears here */}
-          <img src={props.cartItem.imageUrl} className="cart-img" />
-        </div>
-        <div className="cart-item-img">
-          {/* Product Name appears here */}
-          <p>{props.cartItem.name}</p>
-        </div>
-        <div className="cart-item-img">
-          {/* Product Qty appears here */}
-          <form method="post" action="">
-            <input
-              type="text"
-              name="qty"
-              id="qty"
-              className="qty-update"
-              value={props.cartItem.quantity}
-              onChange={() => console.log('qty changed')}
-            />
-            <button type="button">Update</button>
-          </form>
-        </div>
-        <div className="cart-item-img">${props.cartItem.price}.00</div>
-        <div className="cart-item-img">
-          <h4>button placeholder</h4>
+class CartItemSummary extends React.Component {
+  handleQtyUpdate() {
+    //check quantity of item in the cart, change it by what's in the box
+    const newAmt = event.target.content.value
+    console.log('NEW AMOUNT---->', newAmt)
+    this.props.cartItem.quantity = newAmt
+  }
+  render() {
+    let cartItem = this.props.cartItem.experience
+    return (
+      <div>
+        <div className="cart-item">
+          <div className="cart-item-img">
+            {/* Product Image appears here */}
+            <img src={cartItem.imageUrl} className="cart-img" />
+          </div>
+          <div className="cart-item-img">
+            {/* Product Name appears here */}
+            <p>{cartItem.name}</p>
+          </div>
+          <div className="cart-item-img">
+            {/* Product Qty appears here */}
+            <form method="post" action="">
+              <input
+                type="text"
+                name="qty"
+                id="qty"
+                className="qty-update"
+                value={this.props.cartItem.quantity}
+                // onChange={() => console.log('qty changed')}
+              />
+              <button type="button">Update</button>
+            </form>
+          </div>
+          <div className="cart-item-img">
+            ${cartItem.price * this.props.cartItem.quantity}.00
+          </div>
+          <div className="cart-item-img">
+            <h4>button placeholder</h4>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default CartItemSummary
+const mapStateToProps = state => ({
+  activeCart: state.cart
+})
+const mapDispatchToProps = dispatch => ({
+  getCart: () => dispatch(getCart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartItemSummary)
--- client/components/Cart.js
+++ client/components/Cart.js
@@ -6,30 +6,11 @@ import {withStyles} from '@material-ui/core/styles'
 import {connect} from 'react-redux'
 import {getCart} from '../store'
 
-let dummyCartItem = [
-  {
-    id: 1,
-    name: 'Jane Hotel After Midnight',
-    imageUrl:
-      'https://static.urbandaddy.com/uploads/assets/image/articles/standard/cf4eb440aea70a5e56c2453b9e3ce2101debb11e.jpg',
-    duration: '3 hours',
-    description: 'night out',
-    price: 50,
-    quantity: 1,
-    categoryId: 3
-  },
-  {
-    id: 2,
-    name: 'Ballet in Lincoln Center',
-    imageUrl:
-      'https://buildingupontherock.files.wordpress.com/2012/06/tumblr_l9czdphtbi1qdr4zmo1_500.png',
-    duration: '2 hours',
-    description: 'Swan Lake',
-    price: 80,
-    quantity: 2,
-    categoryId: 2
-  }
-]
+const totalCost = cart => {
+  let total = 0
+  cart.map(item => (total += item.experience.price * item.quantity))
+  return `$${total}.00`
+}
 
 class Cart extends React.Component {
   // componentDidMount() {
@@ -65,12 +46,14 @@ class Cart extends React.Component {
           </div>
           {/* THERE WILL BE A MAP OF VALS HERE */}
           {currentCart.map(item => {
+            console.log(item)
             return <CartItemSummary key={item.id} cartItem={item} />
           })}
         </div>
         <div className="cart-subtotal">
           <p>
-            Subtotal: <input type="text" name="subtotal" />
+            Subtotal:{' '}
+            <input type="text" name="subtotal" value={totalCost(currentCart)} />
           </p>
           <p>
             <button type="button">Continue</button>
