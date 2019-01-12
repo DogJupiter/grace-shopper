ef3a04fd12edbbcb6e40b11adc325cfa151684f4
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2019-01-12 15:31:42 -0500

    cart is starting to look less ugly

--- client/components/Cart-Item-Summary.js
+++ client/components/Cart-Item-Summary.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {Grid, Typography, Button} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles'
+import Icon from '@material-ui/core/Icon'
+import IconButton from '@material-ui/core/IconButton'
+import AddIcon from '@material-ui/icons/AddCircle'
 import {connect} from 'react-redux'
 import {
   removeFromCart,
@@ -8,6 +13,38 @@ import {
 } from '../store/cart'
 
 import history from '../history'
+import {BorderAllRounded} from '@material-ui/icons'
+
+const styles = theme => ({
+  content: {
+    textAlign: 'center',
+    justify: 'center',
+    alignItems: 'center',
+    flexGrow: 1,
+    marginTop: '45px'
+  },
+  loader: {
+    fontSize: '35px',
+    marginTop: '50px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    maxWidth: '100%',
+    borderRadius: 16,
+    boxShadow: 3,
+    position: 'left'
+  },
+  cartItemDetails: {
+    fontSize: '20px',
+    fontWeight: 300,
+    align: 'center'
+  },
+  cartItemName: {
+    spacing: 10
+  }
+})
 
 class CartItemSummary extends React.Component {
   handleQtyIncrease() {
@@ -25,44 +62,113 @@ class CartItemSummary extends React.Component {
     history.push('/cart')
   }
   render() {
+    const {classes} = this.props
     let cartItem = this.props.cartItem.experience
-    // console.log('CART ITEM', cartItem)
 
     return (
       <div>
-        <div className="cart-item">
-          <div className="cart-item-img">
-            {/* Product Image appears here */}
-            <img src={cartItem.imageUrl} className="cart-img" />
-          </div>
-          <div className="cart-item-img">
-            {/* Product Name appears here */}
-            <p>{cartItem.name}</p>
-          </div>
-          <div className="cart-item-img">
-            {/* Product Qty appears here */}
-            <p>{this.props.cartItem.quantity}</p>
-            <p>
-              <button type="button" onClick={() => this.handleQtyIncrease()}>
-                Increase
-              </button>
-            </p>
-            <p>
-              <button type="button" onClick={() => this.handleQtyDecrease()}>
-                Decrease
-              </button>
-            </p>
-          </div>
-          <div className="cart-item-img">
-            ${cartItem.price * this.props.cartItem.quantity}.00
-          </div>
-          <div className="cart-item-img">
-            <button type="button" onClick={() => this.handleDeleteClick()}>
-              Delete
-            </button>
-          </div>
+        <div>
+          <Grid container justify="center" spacing={3}>
+            <Grid item xs={2} mx="auto" px="auto" className={classes.content}>
+              <img src={cartItem.imageUrl} className={classes.image} />
+            </Grid>
+            <Grid item xs={2} mx="auto" px="auto" className={classes.content}>
+              <Typography
+                paragraph
+                style={{color: '#627264'}}
+                className={classes.cartItemDetails}
+              >
+                {cartItem.name}
+              </Typography>
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <Typography paragraph style={{color: '#627264'}}>
+                {this.props.cartItem.quantity}
+                <p>
+                  <IconButton
+                    className={classes.icon}
+                    color="secondary"
+                    onClick={() => this.handleQtyIncrease()}
+                  >
+                    <AddIcon />
+                  </IconButton>
+                  <IconButton
+                    className={classes.icon}
+                    color="secondary"
+                    onClick={() => this.handleQtyIncrease()}
+                  >
+                    remove
+                  </IconButton>
+                  {/* <button
+                    type="button"
+                    onClick={() => this.handleQtyIncrease()}
+                  >
+                    Increase
+                  </button>
+                </p>
+                <p>
+                  <button
+                    type="button"
+                    onClick={() => this.handleQtyDecrease()}
+                  >
+                    Decrease
+                  </button> */}
+                </p>
+              </Typography>
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <Typography paragraph style={{color: '#627264'}}>
+                ${cartItem.price * this.props.cartItem.quantity}.00
+              </Typography>
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                className={classes.button}
+                onClick={() => this.handleDeleteClick()}
+              >
+                Delete from Cart
+              </Button>
+            </Grid>
+          </Grid>
         </div>
       </div>
+
+      // <div>
+      //   <div className="cart-item">
+      //     <div className="cart-item-img">
+      //       {/* Product Image appears here */}
+      //       <img src={cartItem.imageUrl} className="cart-img" />
+      //     </div>
+      //     <div className="cart-item-img">
+      //       {/* Product Name appears here */}
+      //       <p>{cartItem.name}</p>
+      //     </div>
+      //     <div className="cart-item-img">
+      //       {/* Product Qty appears here */}
+      //       <p>{this.props.cartItem.quantity}</p>
+      //       <p>
+      //         <button type="button" onClick={() => this.handleQtyIncrease()}>
+      //           Increase
+      //         </button>
+      //       </p>
+      //       <p>
+      //         <button type="button" onClick={() => this.handleQtyDecrease()}>
+      //           Decrease
+      //         </button>
+      //       </p>
+      //     </div>
+      //     <div className="cart-item-img">
+      //       ${cartItem.price * this.props.cartItem.quantity}.00
+      //     </div>
+      //     <div className="cart-item-img">
+      //       <button type="button" onClick={() => this.handleDeleteClick()}>
+      //         Delete
+      //       </button>
+      //     </div>
+      //   </div>
+      // </div>
     )
   }
 }
@@ -83,4 +189,6 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   }
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartItemSummary)
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(CartItemSummary)
+)
--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import CartItemSummary from './Cart-Item-Summary'
-import {Grid} from '@material-ui/core'
+import {Grid, Typography} from '@material-ui/core'
 import {withStyles} from '@material-ui/core/styles'
 
 import {connect} from 'react-redux'
 import {getCart} from '../store/cart'
+
+const styles = theme => ({
+  content: {
+    flexGrow: 1,
+    marginTop: '45px'
+  },
+  loader: {
+    fontSize: '35px',
+    marginTop: '50px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cartHeader: {
+    font: 'Avant Garde',
+    fontSize: '20px',
+    textAlign: 'center',
+    fontWeight: 600
+  }
+})
 
 class Cart extends React.Component {
   // componentDidMount() {
@@ -17,44 +37,71 @@ class Cart extends React.Component {
   }
 
   render() {
+    const {classes} = this.props
     let currentCart = this.props.activeCart.experiences
     if (!currentCart || currentCart.length < 1) {
       return <h1>Nothing in cart</h1>
     }
 
     return (
-      <div className="cart-div">
+      <div>
         <h1>Shopping Cart</h1>
-        <div className="individual-items-view">
-          <div className="cart-item-header">
-            <div>
-              <p>Img</p>
-            </div>
-            <div>
-              <p>Name</p>
-            </div>
-            <div>
-              <p>Qty</p>
-            </div>
-            <div>
-              <p>Price</p>
-            </div>
-            <div>
-              <p>Del</p>
-            </div>
-          </div>
-          {/* THERE WILL BE A MAP OF VALS HERE */}
-          {currentCart.map(item => {
-            console.log(item)
-            return (
-              <CartItemSummary
-                key={item.experience.id}
-                cartItem={item}
-                cartState={this.props.activeCart}
-              />
-            )
-          })}
+        <div>
+          <Grid container justify="center" spacing={3}>
+            <Grid item xs={2} className={classes.content}>
+              {/* <Typography
+                paragraph
+                style={{color: '#627264'}}
+                className={classes.cartHeader}
+              >
+                Image
+              </Typography> */}
+              <div />
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <Typography
+                paragraph
+                style={{color: '#627264'}}
+                className={classes.cartHeader}
+              >
+                Experience
+              </Typography>
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <Typography
+                paragraph
+                style={{color: '#627264'}}
+                className={classes.cartHeader}
+              >
+                Quantity
+              </Typography>
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <Typography
+                paragraph
+                style={{color: '#627264'}}
+                className={classes.cartHeader}
+              >
+                Price
+              </Typography>
+            </Grid>
+            <Grid item xs={2} className={classes.content}>
+              <div />
+            </Grid>
+          </Grid>
         </div>
+        {/* THERE WILL BE A MAP OF VALS HERE */}
+        {currentCart.map(item => {
+          console.log(item)
+          return (
+            <CartItemSummary
+              key={item.experience.id}
+              cartItem={item}
+              cartState={this.props.activeCart}
+            />
+          )
+        })}
+        {/* </div> */}
         <div className="cart-subtotal">
           <p>
             Subtotal:{' '}
@@ -83,7 +130,9 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   }
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cart)
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(Cart)
+)
 
 // const styles = theme => ({
 //   content: {
