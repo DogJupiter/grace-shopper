9115b3ce0e55e26f3029b9a0d20f7e9464c201ac
hilarywenyi <hilarywenyi@gmail.com>
2019-01-13 21:28:27 -0500

    feat: adding landing page

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,30 +1,60 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {user} = props
-  if (!user.id) {
-    return <Redirect to="/" />
+class UserHome extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videoUrl: '/media/SampleVideo1',
+      user: ''
+    }
   }
-  return (
-    <div classNmae="landing-page">
-      <h3>Welcome {user.email}</h3>
-    </div>
-  )
+
+  //export const UserHome = props => {
+  render() {
+    const {user} = this.props
+    console.log('whats this.props', this.props)
+    if (!user.id) {
+      return (
+        <div>
+          <Redirect to="/" />
+          <video autoPlay loop id="video-background" muted plays-inline>
+            <source
+              src="http://techslides.com/demos/sample-videos/small.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <div classNmae="landing-page">
+          <h3>Welcome {user.email}</h3>
+        </div>
+        <video autoPlay loop id="video-background" muted plays-inline>
+          <source
+            src="http://techslides.com/demos/sample-videos/small.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    )
+  }
 }
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapStateToProps = state => {
   return {
     user: state.user
   }
 }
-export default connect(mapState)(UserHome)
+export default connect(mapStateToProps)(UserHome)
 /**
  * PROP TYPES
  */
