f50d2f37db7bfd18cccf3cc4000f1b676f1ac801
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2019-01-15 22:07:18 -0500

    added more seed data

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -8,23 +8,12 @@ const Item = require('./item')
 Review.belongsTo(User)
 Review.belongsTo(Experience)
 Experience.hasMany(Review)
+
 User.hasMany(Order)
 Order.belongsTo(User)
-// Item.belongsTo(Experience)
-// Item.belongsTo(Order)
-// Order.hasMany(Item)
+
 Experience.belongsToMany(Category, {through: 'expCat'})
 Category.belongsToMany(Experience, {through: 'expCat'})
-// Order.hasMany(Experience)
-
-// Order.belongsToMany(Experience, {through: OrderExperience})
-// Experience.belongsToMany(Order, {through: OrderExperience})
-
-// Experience.belongsTo(Category, {through: CategoryExperience})
-// Category.hasMany(Experience)
-// Category.belongsToMany(Experience, {through: CategoryExperience})
-// Experience.belongsTo(Category)
-// Category.hasOne(Experience)
 
 module.exports = {
   User,
